89dfcdb17d105db03cfd56daa316524ac698fab0
hxxtae <fkdlxmfkdl@gmail.com>
2022-03-10 18:53:35 +0900

    Updates

--- src/theme.ts
+++
@@ -1,8 +0,0 @@
-import { DefaultTheme } from 'styled-components';
-
-export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "#f5f6fa",
-  secondColor: "#192a56",
-  accentColor: "#8c7ae6",
-}
